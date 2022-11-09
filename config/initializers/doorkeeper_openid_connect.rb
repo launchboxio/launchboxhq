2896c0a34a24eac3fe104d9a9eeb057aaf2675af
Robert Wittman <robkwittman@gmail.com>
2022-11-09 13:26:15 -0500

    Fix signing key recognition

--- config/initializers/doorkeeper_openid_connect.rb
+++ config/initializers/doorkeeper_openid_connect.rb
@@ -5,7 +5,7 @@ Doorkeeper::OpenidConnect.configure do
     'http://localhost:3000'
   end
 
-  signing_key File.read("private_key.pem")
+  signing_key ENV['OIDC_SIGNING_KEY'] || File.read(ENV['OIDC_SIGNING_KEY_FILE'] || "/certs/private_key.pem")
 
   subject_types_supported [:public]
 
