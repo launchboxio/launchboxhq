f81d2d6bb624b8313e5df0eb425c132e1636cb9b
Robert Wittman <robkwittman@gmail.com>
2022-11-09 20:00:27 -0500

    Fix initializer to grab ENV hostname

--- config/initializers/doorkeeper_openid_connect.rb
+++ config/initializers/doorkeeper_openid_connect.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Doorkeeper::OpenidConnect.configure do
-  issuer do |resource_owner, application|
-    'http://localhost:3000'
+  issuer do |request|
+    ENV['OIDC_DOMAIN'] || "http://localhost:3000"
   end
 
   signing_key ENV['OIDC_SIGNING_KEY'] || File.read(ENV['OIDC_SIGNING_KEY_FILE'] || "/certs/private_key.pem")
