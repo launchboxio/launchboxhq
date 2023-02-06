d7bdae00873c15e24705f75df24db039c0a2ace4
Robert Wittman <robkwittman@gmail.com>
2023-02-06 00:39:24 -0500

    We can officially create a project inside the cluster!

--- app/jobs/projects/create_project_job.rb
+++ app/jobs/projects/create_project_job.rb
@@ -8,8 +8,9 @@ module Projects
     # using the Project composition. This way the vcluster, as well as
     # the provider configuration, is handled as a bundle
     def perform(*args)
-      project = Project.find(args.first)
-      @cluster = Cluster.find(project.cluster_id)
+      @project = Project.find(args.first)
+      @cluster = Cluster.find(@project.cluster_id)
+      @project.update(status: 'provisioning', last_error: nil)
       cert_store = OpenSSL::X509::Store.new
       cert_store.add_cert(OpenSSL::X509::Certificate.new(@cluster.ca_crt))
       @options = {
@@ -20,31 +21,65 @@ module Projects
         }
       }
 
-      # Generate our tempfile
-      Tempfile.create do |f|
-        # TODO: Populate file with values configs
-        f.rewind
-        cli = Rhelm::Client.new(kube_apiserver: "#{@cluster.host}", kube_token: @cluster.token)
-        cli.upgrade(
-          project.slug,
-          'loft-sh/vcluster',
-          values: f.path,
-          wait: true,
-          create_namespace: true,
-          namespace: project.slug,
-          install: true
-        ).run do |lines, status|
-          if status == 0
-            logger.info("helm install worked great!")
-          elsif /timeout/im.match(lines)
-            raise MyTimeoutError, "helm install timed out, oh no!"
-          else
-            # Use the built-in error reporting code to get more details
-            report_failure(lines, status)
-          end
+      create_namespace
+
+      resource = Kubeclient::Resource.new
+      resource.kind = 'Cluster'
+      resource.metadata = {
+        name: @project.slug,
+        namespace: @project.slug
+      }
+      resource.spec = {
+        vcluster: {
+          values: {
+            sync: {
+              ingresses: {
+                enabled: true
+              },
+              serviceaccounts: {
+                enabled: true
+              }
+            },
+            vcluster: {
+              extraArgs: [
+                "--kube-apiserver-arg=--oidc-username-claim=preferred_username"
+              ],
+              resources: {
+                limits: {
+                  cpu: @project.cpu,
+                  memory: "#{@project.memory}Mi"
+                }
+              }
+            },
+            ingress: {
+              enabled: true
+            },
+            # isolation: {
+            #   enabled: true
+            # }
+            storage: {
+              persistence: true,
+              size: "#{@project.disk}Gi"
+            }
+          }
+        }
+      }
+
+      cluster_client = Kubeclient::Client.new("#{@cluster.host}/apis/core.launchboxhq.io", 'v1alpha1', **@options)
+      cluster_client.discover
+
+      begin
+        cluster_client.create_cluster(resource)
+      rescue Kubeclient::HttpError => e
+        if e.error_code == 409
+          cluster_client.merge_patch_cluster(@project.slug, resource, @project.slug)
+        else
+          @project.update(status: 'failed', last_error: e)
+          raise e
         end
       end
 
+      @project.update(status: 'running', last_error: nil)
       # Just do a simple helm install for now
 
       # @cluster.connection_method = "service_account"
@@ -64,14 +99,14 @@ module Projects
       #   puts "Invalid cluster connection_type found, exiting"
       # end
 
-      project.update(status: 'provisioned', last_error: nil)
+      @project.update(status: 'provisioned', last_error: nil)
     end
 
     def create_namespace
       client = Kubeclient::Client.new(@cluster.host, 'v1', **@options)
 
       begin
-        client.create_namespace(Kubeclient::Resource.new({ metadata: { name: project.slug } }))
+        client.create_namespace(Kubeclient::Resource.new({ metadata: { name: @project.slug } }))
       rescue Kubeclient::HttpError => e
         raise e unless e.error_code == 409
         # client.patch_namespace(project.slug, {})
