52dbbc64ef3a596c6bd83e7671b11f185f0ae6db
Robert Wittman <robkwittman@gmail.com>
2023-08-06 20:28:22 -0400

    Move projects controller directory

---
+++ app/controllers/projects/addons_controller.rb
@@ -0,0 +1,11 @@
+module Projects
+  class AddonsController < Projects::ApplicationController
+    def index; end
+    def show; end
+    def new; end
+    def edit; end
+    def update; end
+    def create; end
+    def destroy; end
+  end
+end
