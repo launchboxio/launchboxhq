59ce53bd4afe6ded7b3beedccca177ee8a3d872b
Robert Wittman <robkwittman@gmail.com>
2023-08-08 22:39:57 -0400

    Add note about json schemas

--- db/migrate/20230808234155_create_addon_versions.rb
+++ db/migrate/20230808234155_create_addon_versions.rb
@@ -6,6 +6,8 @@ class CreateAddonVersions < ActiveRecord::Migration[7.0]
       t.string :claim_name
       t.boolean :default
       t.string :group
+      # We no longer need the json_schema field(s)
+      # The OpenAPI schema contains it as a property
       t.json :schema
 
       t.timestamps
