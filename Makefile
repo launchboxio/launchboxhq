a590f8f1c6a487bd709674f7408fb5d06062003f
Robert Wittman <robkwittman@gmail.com>
2023-10-30 09:05:36 -0400

    Migrate to action cable for cluster events

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ rubocop:
 
 rspec:
 	RAILS_ENV=test bundle exec rspec --fail-fast
+
+rails:
+	bundle exec puma -t 1:1 -b tcp://0.0.0.0:3000
--- app/channels/cluster_channel.rb
+++ app/channels/cluster_channel.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
+# AddonsChannel is a cluster scoped channel,
+# which emits the following events to the
+# reuired clusters
+# - projects.created
+# - projects.updated
+# - projects.paused
+# - projects.resumed
+# - projects.deleted
+# - addons.created
+# - addons.updated
+# - addons.deleted
 class ClusterChannel < ApplicationCable::Channel
   def subscribed
-    reject unless cluster
-    stream_for cluster
+    puts params[:cluster_id]
+    @cluster = Cluster.find(params[:cluster_id])
+    stream_for @cluster
   end
 end
