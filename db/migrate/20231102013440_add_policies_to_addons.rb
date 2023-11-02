db39cc3d3922752b03d7b0c5e4a486973fef156d
Robert Wittman <robkwittman@gmail.com>
2023-11-02 09:05:00 -0400

    Remove policies addons migration

--- db/migrate/20231102013440_add_policies_to_addons.rb
+++
@@ -1,6 +0,0 @@
-class AddPoliciesToAddons < ActiveRecord::Migration[7.0]
-  def change
-    add_column :addons, :activation_policy, :string
-    add_column :addons, :pull_policy, :string
-  end
-end
