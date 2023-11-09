d2944e899ede3bde467af10570982b63704a4be3
Robert Wittman <robkwittman@gmail.com>
2023-11-08 20:49:50 -0500

    fix: Disable usage of managed clusters for now

--- app/services/clusters/create_cluster_service.rb
+++ app/services/clusters/create_cluster_service.rb
@@ -7,7 +7,6 @@ module Clusters
       application = Doorkeeper::Application.create!(name: SecureRandom.uuid, confidential: true, redirect_uri: 'https://localhost:8080')
       @cluster.oauth_application = application
       @cluster.save!
-      Clusters::CreateClusterJob.perform_later(@cluster.id) if @cluster.managed?
       true
     end
   end
