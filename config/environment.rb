42c6f54b7f75bc32b72e56d2c83d4d94a6d69933
Robert Wittman <robkwittman@gmail.com>
2023-11-16 21:35:20 -0500

    fix: Move action_mailer to application.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'action_mailer'
-
 ActionMailer::Base.smtp_settings = {
   user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
   password: ENV.fetch('SENDGRID_API_KEY', nil), # This is the secret sendgrid API key which was issued during API key creation
